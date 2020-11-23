0dee20ec36a5e31746dfd08f2f895b2be7c794c1
IRINA VOLKHONSKAYA <irina.volhonskaya@gmail.com>
2020-11-23 21:52:49 +0200

    first success test of component Logo

--- react-practice/src/components/Logo/Logo.test.js
+++ react-practice/src/components/Logo/Logo.test.js
@@ -1,44 +1,17 @@
 import React from "react";
 import Logo from "./Logo";
-import chai from 'chai'
-import chaiEnzyme from 'chai-enzyme'
-import { shallow, mount, render } from "enzyme";
+
+import { mount, shallow } from "enzyme";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
-chai.use(chaiEnzyme());
 
 describe("<Logo />", () => {
   it("renders <Logo /> component", () => {
-    const wrapper = shallow(
-      <Logo>
-        <img className="logo" />
-      </Logo>
-      
-    );
-    console.log(wrapper.debug());
+    const wrapper = mount(<img className="logo" />);
+
     expect(wrapper.contains(<img className="logo" />)).toEqual(true);
   });
+
 });
-
-// describe("<Logo />", () => {
-//   it("passing test", () => {
-//     expect(true).toBeTruthy();
-//   });
-// });
-
-// import React from "react";
-// import AppHeader from "./AppHeader";
-//
-
-// describe("<MyComponent />", () => {
-//   it("renders children when passed in", () => {
-//     const wrapper = shallow(
-//       <AppHeader>
-//         <div className="unique" />
-//       </AppHeader>
-//     );
-//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
-//   });
-// });
