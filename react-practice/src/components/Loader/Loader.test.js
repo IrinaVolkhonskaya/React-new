4b66f9431cc935e19dfcf2326f33889fede125c4
IRINA VOLKHONSKAYA <irina.volhonskaya@gmail.com>
2020-11-24 10:50:27 +0200

    added testing with snapshort in Loader

---
+++ react-practice/src/components/Loader/Loader.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Loader from "./Loader";
+
+import { shallow } from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("<Logo />", () => {
+  it("render correctly Loader component", () => {
+    const LoaderComponent = shallow(<Loader />);
+    expect(LoaderComponent).toMatchSnapshot();
+  });
+
+  it("check h1 container by default", () => {
+    const LoaderComponent = shallow(<Loader />);
+    expect(LoaderComponent.find("h1").text()).toEqual("Loading...");
+  });
+});
--- react-practice/src/components/Logo/Logo.test.js
+++ react-practice/src/components/Logo/Logo.test.js
@@ -14,4 +14,28 @@ describe("<Logo />", () => {
     expect(wrapper.contains(<img className="logo" />)).toEqual(true);
   });
 
+  // it('renders children when passed in', () => {
+  //   const wrapper = shallow((
+  //     <Logo>
+  //       <img className="logo" />
+  //     </Logo>
+  //   ));
+  //   expect(wrapper.contains(<img className="logo" />)).toEqual(true);
+  // });
+
 });
+
+// import React from "react";
+// import AppHeader from "./AppHeader";
+//
+
+// describe("<MyComponent />", () => {
+//   it("renders children when passed in", () => {
+//     const wrapper = shallow(
+//       <AppHeader>
+//         <div className="unique" />
+//       </AppHeader>
+//     );
+//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+//   });
+// });
