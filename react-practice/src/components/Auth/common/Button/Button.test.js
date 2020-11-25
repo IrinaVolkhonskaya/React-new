02a009424821c026e5d66eb6dfd549f46953b934
IRINA VOLKHONSKAYA <irina.volhonskaya@gmail.com>
2020-11-25 12:58:45 +0200

    test onClick

---
+++ react-practice/src/components/Auth/common/Button/Button.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "./Button";
+import { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Test Button component", () => {
+  it("should click event", () => {
+    const mockCallBack = jest.fn();
+
+    const button = shallow(<Button onClick={mockCallBack}>Ok!</Button>);
+    button.find("button").simulate("click");
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  });
+});
